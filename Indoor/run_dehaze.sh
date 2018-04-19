531dc931c88de73dc477e8c620b2e36ba8a91549
He Zhang <he.zhang92@rutgers.edu>
2018-04-19 22:39:02 +0800

    Add files via upload

---
+++ Indoor/run_dehaze.sh
@@ -0,0 +1,2 @@
+python dehazetest.py --dataroot ./sample/indoortest2 --valDataroo ./sample/indoortest2 --netG ./pretrained-model/netG_epoch_23000.pth
+python dehazetest1.py --dataroot ./sample/indoortest3 --valDataroo ./sample/indoortest3 --netG ./pretrained-model/netG_epoch_23000.pth
